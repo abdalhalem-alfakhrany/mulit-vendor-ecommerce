2115c83b41680ff6e0cbf44ce92b79bc670b2f69
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-07-10 12:35:20 +0300

    a command for specify migrations order (not used )

---
+++ app/Console/Commands/MigrateInOrder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class MigrateInOrder extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'migrate_ordered';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $migrations = [
+            '2023_07_08_153017_create_images_table.php',
+            '2023_07_08_153039_create_users_table.php',
+        ];
+
+        foreach ($migrations as $migration) {
+            $basePath = 'database/migrations/';
+            $migrationName = trim($migration);
+            $path = $basePath . $migrationName;
+            $this->call('migrate:refresh', [
+                '--path' => $path,
+            ]);
+        }
+    }
+}
