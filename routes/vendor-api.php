e5b6418caed12576441ff0d0bef10f307dd8b125
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-07-15 13:54:26 +0300

    new routs

--- routes/vendor-api.php
+++ routes/vendor-api.php
@@ -1,8 +1,8 @@
 <?php
 
-use App\Http\Controllers\Api\Vendor\OrdersController;
-use App\Http\Controllers\Api\Vendor\ProductsController;
+use App\Http\Controllers\Api\Vendor\VendorOrdersController;
+use App\Http\Controllers\Api\Vendor\VendorProductsController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('products',  ProductsController::class);
-Route::resource('orders', OrdersController::class)->only(['index', 'show', 'update']);
+Route::apiResource('products',  VendorProductsController::class);
+Route::apiResource('orders', VendorOrdersController::class)->only(['index', 'show', 'update']);
