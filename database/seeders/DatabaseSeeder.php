d16cbd89bc853b975b1b8575c9503c5958d7b88a
abdalhalem-alfakhrany <abdalhalem.hosni.alfakhrany@gmail.com>
2023-07-15 13:55:35 +0300

    update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,7 +6,7 @@ namespace Database\Seeders;
 
 use App\Models\Order;
 use App\Models\Product;
-use App\Models\ShoppingList;
+use App\Models\ShoppingCart;
 use App\Models\Tag;
 use App\Models\User;
 use App\Models\Vendor;
@@ -20,21 +20,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $users = [];
         for ($i = 1; $i <= 10; $i++) {
-            array_push(
-                $users,
-                User::create([
-                    'first_name' => 'user ',
-                    'last_name' => '' . $i,
-                    'email' => 'user' . $i . '@app.com',
-                    'password' => Hash::make('password')
-                ])
-            );
+            $user = User::create([
+                'first_name' => 'user ',
+                'last_name' => '' . $i,
+                'email' => 'user' . $i . '@app.com',
+                'password' => Hash::make('password')
+            ]);
+            $token = $user->createToken('my_token')->plainTextToken;
+            echo "$token\n";
         }
-
-        $token = $users[0]->createToken('my_token')->plainTextToken;
-        echo "$token\n";
 
         for ($i = 1; $i <= 3; $i++) {
             Vendor::create([
@@ -50,29 +45,33 @@ class DatabaseSeeder extends Seeder
         }
 
         for ($i = 0; $i < 10; $i++) {
+            $price = rand(100, 500);
+            $is_offered = rand(0, 1);
+
             $product = Product::create([
                 'name' => 'product ' . $i,
                 'description' => 'product ' . $i . ' description',
-                'price' => rand(100, 500),
-                'vendor_id' => 1,
+                'price' => $price,
+                'offer_price' => ($is_offered) ? rand($price - 100, $price) : null,
+                'vendor_id' => Vendor::inRandomOrder()->limit(1)->get(['id'])->first()->id,
             ]);
             $product->tags()->save(Tag::inRandomOrder()->limit(1)->get(['id'])->first());
         }
 
-        $shopping_list = ShoppingList::create([
-            'name' => 'shopping list test',
-            'user_id' => 1
-        ]);
+        for ($i = 0; $i < 10; $i++) {
+            $shopping_cart = ShoppingCart::create([
+                'name' => 'shopping cart' . $i,
+                'user_id' => User::inRandomOrder()->limit(1)->get(['id'])->first()->id
+            ]);
 
-        $products = Product::inRandomOrder(4)->get();
-
-        for ($i = 0; $i < 4; $i++) {
-            $shopping_list->products()->save($products[$i]);
+            Product::inRandomOrder()->limit(2)->get()->each(function ($product) use ($shopping_cart) {
+                $shopping_cart->products()->save($product);
+            });
         }
 
-        $order = Order::create([
-            'vendor_id' => 1,
-            'shopping_list_id' => 1,
-        ]);
+        // $order = Order::create([
+        //     'vendor_id' => 1,
+        //     'shopping_cart_id' => 1,
+        // ]);
     }
 }
